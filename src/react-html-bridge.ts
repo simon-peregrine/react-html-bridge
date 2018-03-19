e9fa4e970972408699f26d9d398d6de294d509da
Simon Broadhead <simon@peregrinelabs.com>
2018-03-19 14:02:10 -0400

    Pass context to props function

--- src/react-html-bridge.ts
+++ src/react-html-bridge.ts
@@ -5,7 +5,7 @@ import * as ReactDOM from 'react-dom';
  * Find all elements with a `data-component` attribute and attempt to mount the specified React component in them.
  * The available components must be specified in the `components` argument.
  *
- * Initial props can be specified by creating a hidden (display: none) div somewhere on the page with a unique ID
+ * Initial props can be specified by creating a script with type="text/plain" somewhere on the page with a unique ID
  * containing Javascript code (properly escaped to avoid XSS attacks if it contains user-supplied input) that evaluates
  * to a props object. The container component can then specify the `data-props-id` attribute to be the ID of the div
  * containing the props.
@@ -25,6 +25,10 @@ export default function reactHTMLBridge(components: [React.ClassType<any, any, a
       return;
     }
 
+    let context = {
+      components: componentMap
+    };
+
     let initialProps: object = {};
     let propsId: string = container.dataset.propsId || '';
     if (propsId) {
@@ -32,12 +36,13 @@ export default function reactHTMLBridge(components: [React.ClassType<any, any, a
       if (propsElem !== null) {
         let propsJs: string = propsElem.innerText;
         try {
-          initialProps = eval(`(${propsJs})`);
+          const evalFunc = (code: string) => eval(code);
+          initialProps = evalFunc.call(context, propsJs);
         } catch (e) {
-          console.error(e);
+          console.warn(`Could not load props for React component ${componentName} with propsId ${propsId} due to an error:\n ${e.message}`);
         }
       } else {
-        console.warn(`Could not load props for React component ${componentName} with propsId ${propsId}`);
+        console.warn(`No such props container: ${propsId}`);
       }
     }
 
